87f5d71c4d729ee67af067ac01d746465768f7fb
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-01-20 14:05:14 -0800

    added more backbones

--- model.py
+++ model.py
@@ -3,16 +3,44 @@ import torchvision
 import torchvision.transforms as transforms
 import torchvision.models as models
 
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.models as models
+
 class Model(torch.nn.Module):
-    def __init__(self, backbone='resnet50'):
+    def __init__(self, backbone='resnet50', pretrained=True):
         super(Model, self).__init__()
+
+        #  we'll have to substitute the classifier here
         if backbone == 'resnet18':
-            self.backbone = models.resnet18(pretrained=True)
+            self.backbone = models.resnet18(pretrained=pretrained)
             self.backbone.fc = torch.nn.Linear(512,1)
 
         elif backbone == 'resnet50':
-            self.backbone = models.resnet50(pretrained=True)
-            self.backbone.fc = torch.nn.Linear(2048,1) #  we're substituting the classifier here
+            self.backbone = models.resnet50(pretrained=pretrained)
+            self.backbone.fc = torch.nn.Linear(2048,1) 
+
+        elif backbone == 'resnet34':
+            self.backbone = models.resnet34(pretrained=pretrained)
+            self.backbone.fc = torch.nn.Linear(512,1)
+
+        elif backbone == 'resnet101':
+            self.backbone = models.resnet101(pretrained=pretrained)
+            self.backbone.fc = torch.nn.Linear(2048,1) 
+
+        elif backbone == 'mobilenet':
+            self.backbone = models.mobilenet.mobilenet_v2(pretrained=pretrained)
+            self.backbone.classifier = torch.nn.Sequential(
+                torch.nn.Dropout(p=0.2, inplace=False),
+                torch.nn.Linear(in_features=1280, out_features=1, bias=True)
+                )
+            
+        elif backbone == 'densenet121':
+            self.backbone = models.densenet121(pretrained=pretrained)
+            self.backbone.classifier = torch.nn.Linear(in_features=1024, out_features=1, bias=True)
+        
+        else: raise NotImplementedError
         
         self.sigmoid = torch.nn.Sigmoid()
         
