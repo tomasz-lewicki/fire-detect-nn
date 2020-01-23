495d4e08ef2b10e48226668e207bf5dcf74d55bd
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-01-23 01:30:50 -0800

    updated inference benchmarks

--- inference.py
+++ inference.py
@@ -1,29 +1,42 @@
 import torch
+import os
+import time
+
 from model import Model
 
-model = Model(backbone='resnet18')
-model = torch.load('firedetection-resnet18.pt')
-device = torch.device("cuda:0")
-model = model.to(device)
+weight_paths = os.listdir('weights')
 
 def avgerage(l):
     return sum(l)/len(l)
 
-for param in model.parameters():
-    param.requires_grad = False
+device = torch.device("cuda:0")
+fps_results = {}
 
-import time
-times = []
+for p in weight_paths:
+    backbone_name = p.split('-')[0]
+    print(f'trying {backbone_name}')
+    model = Model(backbone=backbone_name)
+    model = torch.load('weights/'+p)
+    model = model.to(device)
 
-for _ in range(50):
+    for param in model.parameters():
+        param.requires_grad = False
+
+    times = []
     tens = torch.rand((1,3,224,224)).to(device)
-    start = time.time()
+    model(tens)
 
-    result = model(tens)
+    for _ in range(50):
+        tens = torch.rand((1,3,224,224)).to(device)
+        start = time.time()
 
-    timing = time.time()-start
-    print(timing)
-    times.append(timing)
+        result = model(tens)
 
+        timing = time.time()-start
+        print(timing)
+        times.append(timing)
 
-print(f'average inference: {avgerage(times)}')
+    fps = round(1/avgerage(times), 2)
+
+    fps_results[backbone_name] = fps
+    print(f'average fps with {backbone_name} : {fps}')
