89bef286d2249bac1d00cb5a94edf05da9160f9e
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-01-30 14:40:16 -0800

    added shuffling flag to dataset

--- model.py
+++ model.py
@@ -48,9 +48,10 @@ class Model(torch.nn.Module):
         return x
 
 def load_dataset(directory='~/fire_aerial2k_dataset/',
-                     val_frac = 0.1,
-                     batch_size = 16,
-                     random_seed = 4822):
+                 val_frac = 0.1,
+                 batch_size = 16,
+                 random_seed = 4822,
+                 shuffle=True):
     
     tr = torchvision.transforms.Compose([torchvision.transforms.Resize((224,224)),
                                 torchvision.transforms.ToTensor()])
@@ -63,7 +64,9 @@ def load_dataset(directory='~/fire_aerial2k_dataset/',
     indices = list(range(n_all))
 
     np.random.seed(random_seed)
-    np.random.shuffle(indices)
+    
+    if shuffle:
+        np.random.shuffle(indices)
 
     train_idxs_list, test_idxs_list = indices[n_valid:], indices[:n_valid]
 
