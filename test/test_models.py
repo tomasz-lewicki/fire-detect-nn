268a5ee33d81bb001db0740dbdc263ba3e41154d
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-04-27 18:50:05 -0700

    added tests

---
+++ test/test_models.py
@@ -0,0 +1,19 @@
+
+from model import Model
+import torch
+import unittest
+
+class TestModels(unittest.TestCase):
+
+    def test_backbone_avail(self):
+        BACKBONES = ['resnet18','resnet34','resnet50','resnet101', 'densenet121']
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        for b in BACKBONES:
+            print(b)
+            model = Model(backbone=b)
+            model = model.to(device)
+            self.assertTrue(model(torch.ones(1,3,224,224).to(device)))
+
+
+if __name__ == '__main__':
+    unittest.main()
