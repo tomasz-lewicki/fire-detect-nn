5977f7d9f9a29caf8902d12c9fe55e304d34f13e
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-03-08 17:54:53 -0800

    Print the total # of batches in info message

--- train.py
+++ train.py
@@ -100,7 +100,7 @@ for epoch in range(EPOCHS):
         if i % PRINT_EVERY == 0:
             print(
                 f"epoch: {epoch+1:03d} "
-                f"batch: {i:05d} "
+                f"batch: {i:05d}/{len(train) * BATCH_SIZE} "
                 f"loss: {np.mean(running_loss):.3f} "
                 f"training accuracy: {np.mean(running_acc):.3f} "
             )
